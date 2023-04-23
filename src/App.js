da0ce6aad1a8616b5f6b64520aa94e699aa14ade
AhmadLAhmad <ahmadd.l.ahmadd1998@gmail.com>
2023-04-23 22:36:46 +0300

    installing pakages

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Header } from "./components/Header";
-import { Footer } from "./components/Footer";
+// import { Footer } from "./components/Footer";
 import { Home } from "./pages/Home";
 import { Gallery } from "./pages/Gallery";
 import { About } from "./pages/About";
