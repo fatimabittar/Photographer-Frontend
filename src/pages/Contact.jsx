a8272892cca5d2b727940e1a0055a6d77601a7cc
nour <nour.hmadi.a@gmail.com>
2023-05-01 02:28:17 +0300

    unsaved changes

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,12 +1,26 @@
-import React from 'react'
-import AboutHeader from '../components/About/AboutHeader/AboutHeader'
-import ba from '../components/About/AboutHeader/ba.jpg'
+import React from "react";
+import AboutHeader from "../components/About/AboutHeader/AboutHeader";
+import ba from "../components/About/AboutHeader/ba.jpg";
+import "../styles/Contact.css";
 
 export const Contact = () => {
   return (
     <div>
- 
-       <AboutHeader backgroundImage={ba} />
+      <AboutHeader backgroundImage={ba} />
+      <div className="contact-container">
+        <div className="contact-details">
+          <h3>Contact Details</h3>
+
+          <div className="contact-card">
+            <div className="contact-icon"></div>
+            <div>
+              <h5>Call us</h5>
+              <p>phone nuber</p>
+            </div>
+          </div>
+        </div>
+        <div className="contact-form"></div>
+      </div>
     </div>
-  )
-}
+  );
+};
